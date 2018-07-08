39950c96638937e0310a29c18c931869b3079be3
吹田洋輔 <suita@mba-2.local>
2018-07-08 17:04:38 +0900

    Setting sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,11 +74,11 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.smtp_settings = {
-  :address => 'smtp.gmail.com',
+  :address => 'smtp.sendgrid.net',
   :port => 587,
-  :domain => 'example.com',
-  :user_name => 'yohsuke.suita@gmail.com',
-  :password => 'yohsuke919',
+  :domain => 'heroku.com',
+  :user_name => ENV['SENDGRID_USERNAME'],
+  :password => ENV['SENDGRID_PASSWORD'],
   :authentication => :plain,
   :enable_starttls_auto => true
 }
