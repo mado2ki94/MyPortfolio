47df55a405cd71e8d535abdff04fbb86c9e3eb71
吹田洋輔 <suita@mba-2.local>
2018-07-08 16:13:07 +0900

    Modify config production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,8 +77,8 @@ Rails.application.configure do
   :address => 'smtp.gmail.com',
   :port => 587,
   :domain => 'example.com',
-  :user_name => ENV['yohsuke.suita@gmail.com'],
-  :password => ENV['yohsuke919'],
+  :user_name => ENV['GMAIL_USERNAME'],
+  :password => ENV['GMAIL_PASSWORD'],
   :authentication => :plain,
   :enable_starttls_auto => true
 }
